401a735fcda6106ea115c31d82bf7dff3522a983
Sindbad <88231729+04312581464@users.noreply.github.com>
2024-12-29 21:58:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const prevYear = document.getElementById('prevYear');
+const newYear = document.getElementById('newYear');
+
+// Update the logic for the year
+prevYear.innerHTML = 4; // Update for '2024' (last digit '4')
+newYear.innerHTML = 5;  // Update balloon to display '5'
